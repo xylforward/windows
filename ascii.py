9e8b44eb953c2f0ace3275d5c8f8f5707283bf94
xiao <xiaoyanlong98@gmail.com>
2019-01-23 19:58:39 +0800

    hello

--- ascii.py
+++ ascii.py
@@ -1,52 +1,12 @@
 from PIL import Image
-import argparse
 
-#命令行输入参数处理
-parser = argparse.ArgumentParser()
-
-parser.add_argument('file')     #输入文件
-parser.add_argument('-o', '--output')   #输出文件
-parser.add_argument('--width', type = int, default = 80) #输出字符画宽
-parser.add_argument('--height', type = int, default = 80) #输出字符画高
-
-#获取参数
-args = parser.parse_args()
-
-IMG = args.file
-WIDTH = args.width
-HEIGHT = args.height
-OUTPUT = args.output
-
-ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
-
-# 将256灰度映射到70个字符上
-def get_char(r,g,b,alpha = 256):
-    if alpha == 0:
-        return ' '
-    length = len(ascii_char)
-    gray = int(0.2126 * r + 0.7152 * g + 0.0722 * b)
-
-    unit = (256.0 + 1)/length
-    return ascii_char[int(gray/unit)]
-
-if __name__ == '__main__':
-
-    im = Image.open(IMG)
-    im = im.resize((WIDTH,HEIGHT), Image.NEAREST)
-
-    txt = ""
-
-    for i in range(HEIGHT):
-        for j in range(WIDTH):
-            txt += get_char(*im.getpixel((j,i)))
-        txt += '\n'
-
-    print(txt)
-    
-    #字符画输出到文件
-    if OUTPUT:
-        with open(OUTPUT,'w') as f:
-            f.write(txt)
-    else:
-        with open("output.txt",'w') as f:
-            f.write(txt)
+# 打开一个jpg图像文件，注意是当前路径:
+im = Image.open('IMG_20170906_210135.jpg')
+# 获得图像尺寸:
+w, h = im.size
+print('Original image size: %sx%s' % (w, h))
+# 缩放到50%:
+im.thumbnail((w//2, h//2))
+print('Resize image to: %sx%s' % (w//2, h//2))
+# 把缩放后的图像用jpeg格式保存:
+im.save('thumbnail.jpg', 'jpeg')
